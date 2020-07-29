bf58e146f09666b0cf5ea57efbfd2477c26d3732
Puripant <puripant@gmail.com>
2020-07-30 00:18:58 +0700

    Support custom scaling

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const maxWidth = 500;
+const scaling = 2;
 
 let option = 'grabcut';
 let input, src, dst, dst2;
@@ -14,7 +15,7 @@ imgElement.onload = () => {
   cv.resize(input, input, new cv.Size(width, height), 0, 0, cv.INTER_AREA);
 
   src = input.clone();
-  cv.resize(src, src, new cv.Size(width/2, height/2), 0, 0, cv.INTER_AREA);
+  cv.resize(src, src, new cv.Size(width/scaling, height/scaling), 0, 0, cv.INTER_AREA);
   cv.cvtColor(src, src, cv.COLOR_RGBA2RGB, 0);
 
   update(option);
@@ -40,9 +41,11 @@ function grabcut() {
   let mask = new cv.Mat();
   let bgdModel = new cv.Mat();
   let fgdModel = new cv.Mat();
-  // let rect = new cv.Rect(100, 50, 300, 800); //running-man
-  // let rect = new cv.Rect(200, 0, 300, 800); //running-woman
-  let rect = new cv.Rect(50/2, 100/2, 400/2, 1000/2);
+  // let rect = new cv.Rect(100/scaling, 50/scaling, 200/scaling, 800/scaling); //running-man
+  // let rect = new cv.Rect(80/scaling, 50/scaling, 300/scaling, 800/scaling); //running-pair //bad result
+  // let rect = new cv.Rect(200/scaling, 0/scaling, 150/scaling, 800/scaling); //running-woman //bad result
+  let rect = new cv.Rect(80/scaling, 100/scaling, 320/scaling, 800/scaling); //workout
+  // let rect = new cv.Rect(50/scaling, 100/scaling, 400/scaling, 1000/scaling); //goongging
   cv.grabCut(src, mask, rect, bgdModel, fgdModel, 2, cv.GC_INIT_WITH_RECT);
 
   dst = input.clone()
@@ -50,10 +53,11 @@ function grabcut() {
     for (let j = 0; j < src.cols; j++) {
       // obvious & possible background; 1 & 3 for obvious and possible foreground
       if (mask.ucharPtr(i, j)[0] == 0 || mask.ucharPtr(i, j)[0] == 2) {
-        dst.ucharPtr(2*i  , 2*j  )[3] = 0;
-        dst.ucharPtr(2*i  , 2*j+1)[3] = 0;
-        dst.ucharPtr(2*i+1, 2*j  )[3] = 0;
-        dst.ucharPtr(2*i+1, 2*j+1)[3] = 0;
+        for (let x = 0; x < scaling; x++) {
+          for (let y = 0; y < scaling; y++) {
+            dst.ucharPtr(scaling*i + x, scaling*j + y)[3] = 0;
+          }
+        }
       }
     }
   }
@@ -64,8 +68,8 @@ function grabcut() {
   fgdModel.delete();
 
   let color = new cv.Scalar(0, 0, 255);
-  let point1 = new cv.Point(rect.x*2, rect.y*2);
-  let point2 = new cv.Point(rect.x*2 + rect.width*2, rect.y*2 + rect.height*2);
+  let point1 = new cv.Point(rect.x*scaling, rect.y*scaling);
+  let point2 = new cv.Point(rect.x*scaling + rect.width*scaling, rect.y*scaling + rect.height*scaling);
   cv.rectangle(input, point1, point2, color);
   cv.imshow('background', input);
 }
